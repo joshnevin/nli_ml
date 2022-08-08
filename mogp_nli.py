64a2ebd535b18a02fc20cb2f5d6f56a830cd9d27
JoshWNevin <jn399@cam.ac.uk>
2022-08-08 14:34:08 +0100

    updated mean function to include correct physics (in notebook)

--- mogp_nli.py
+++ mogp_nli.py
@@ -23,6 +23,7 @@ num_test = 100
 kernel_function = 'SquaredExponential'
 
 nugget_type = "fit"
+mf = "I(x[0]**3) + I(x[1]**3) + I(x[2]**3) + I(x[3]**3) + I(x[4]**3)"
 
 
 noise_powers_shaped = np.zeros([num_channels,num_examples])
@@ -33,14 +34,19 @@ if __name__ == "__main__": # this is required for multiprocessing to work correc
 
 # Next, fit the surrogate MOGP model using MAP with the default priors
 
-    gp = mogp_emulator.MultiOutputGP(launch_powers[:150], noise_powers_shaped[:,:150],
-    nugget=nugget_type, kernel=kernel_function, mean="x[0]+x[1]+x[2]+x[3]+x[4]")
+    gp = mogp_emulator.MultiOutputGP(launch_powers[:150], noise_powers_shaped[:,:150], mean=mf,
+    nugget = nugget_type, kernel = kernel_function)
     start = time.time()
     gp = mogp_emulator.fit_GP_MAP(gp, n_tries=2)
     end = time.time()
     print(end-start)
-    # pickle.dump(gp, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+".pkl", 'wb'))
-    # pickle.dump(end-start, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+"_traintime.pkl", 'wb'))
+    print(gp.emulators[0].theta)
+    print(gp.emulators[1].theta)
+    print(gp.emulators[2].theta)
+    print(gp.emulators[3].theta)
+    print(gp.emulators[4].theta)
+    pickle.dump(gp, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+"_mf.pkl", 'wb'))
+    pickle.dump(end-start, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+"_mf_traintime.pkl", 'wb'))
 
 # Validate emulator by comparing to true simulated value
 # To compare with the emulator, use the predict method to get mean and variance
@@ -50,5 +56,5 @@ if __name__ == "__main__": # this is required for multiprocessing to work correc
     predictions = gp.predict(launch_powers[150:])
     end = time.time()
     print(end-start)
-    # pickle.dump(predictions, open("results/predictions_100_"+kernel_function+"_"+nugget_type+".pkl", 'wb'))
-    # pickle.dump(end-start, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+"_testtime.pkl", 'wb'))
+    pickle.dump(predictions, open("results/predictions_100_"+kernel_function+"_"+nugget_type+"_mf.pkl", 'wb'))
+    pickle.dump(end-start, open("results/gp_150"+"_"+kernel_function+"_"+nugget_type+"_mf_testtime.pkl", 'wb'))
